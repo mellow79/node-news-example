70de65bacff9d113304f82464c24e181e0c343b5
mlowery <mlow7920@gmail.com>
2023-12-07 01:14:51 -0500

    added some changes

--- fetchNews.js
+++ fetchNews.js
@@ -1,26 +1,26 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 const fetchNews = async () => {
-  const apiUrl =
-    'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4a0e337df2df442dac38476d8c9ea4d7';
-
   try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
+    const response = await axios.get('https://newsapi.org/v2/top-headlines', {
+      params: {
+        country: 'us',
+        category: 'business',
+        apiKey: '4a0e337df2df442dac38476d8c9ea4d7',
+      },
+    });
 
-    if (data.status === 'ok') {
-      console.log('Top Business Headlines (US):');
-      data.articles.forEach((article, index) => {
-        console.log(`\n${index + 1}. ${article.title}`);
-        console.log(`   ${article.description}`);
-        console.log(`   Read more: ${article.url}\n`);
-      });
-    } else {
-      console.error('Error:', data.message);
-    }
+    const articles = response.data.articles;
+    articles.forEach((article) => {
+      console.log(`Title: ${article.title}`);
+      console.log(`Description: ${article.description}`);
+      console.log(`URL: ${article.url}`);
+      console.log('------------------------');
+    });
   } catch (error) {
     console.error('Error fetching news:', error.message);
   }
 };
 
+// Call the function to fetch news
 fetchNews();
